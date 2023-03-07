9af61a05b53af1f6501a34a4e0c6d31acb2de8c7
Vikram Sehra <vikramkr.meena.min16@itbhu.ac.in>
2023-03-07 14:45:54 +0530

    my first commit to react tech news website

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Search from "./Search";
+import Pagination from "./Pagination";
+import Stories from "./Stories";
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Search />
+      <Pagination />
+      <Stories />
+    </div>
+  )
+}
+
+export default App
